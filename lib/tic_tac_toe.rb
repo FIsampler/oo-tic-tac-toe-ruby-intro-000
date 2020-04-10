b6e20cdc43f74532d0a9d22ecc63988760374b3a
Max Bonus <mguzenko@outlook.com>
2020-04-10 01:01:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,7 +76,7 @@ class TicTacToe
   end
 
   def turn()
-    puts "Please enter 1-9:"
+    # puts "Please enter 1-9:"
     user_input = gets.strip
     index=input_to_index(user_input)
     if !valid_move?(index)
@@ -87,7 +87,7 @@ class TicTacToe
   end
 
   def play()
-    if over?()==false
+    if !over?
       puts "Please enter 1-9:"
       turn(user_input)
       play()
